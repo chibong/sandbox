234092285b85a0a7951e86823688ad40cc920a7e
Chibong <chibong@gmail.com>
2013-12-08 09:40:21 -0800

    styled flash messages

--- projects/sandbox/test/models/user_test.rb
+++ projects/sandbox/test/models/user_test.rb
@@ -4,4 +4,7 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "a user should enter a first name" do
+  	user = User.new
+  	assert !user.save
 end
